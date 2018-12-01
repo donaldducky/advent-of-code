87b188f29fc7e3a2301aca9a5fe7284bf4b3b4fe
Donald Chea <donaldducky@gmail.com>
2018-12-01 15:12:18 -0500

    Simplify 1b
    After learning about Stream.cycle() and Enum.reduce_while()

--- 2018/day1.exs
+++ 2018/day1.exs
@@ -1,4 +1,4 @@
-# elixir day1.exs < day1-input.txt
+# Usage: elixir day1.exs < day1-input.txt
 IO.stream(:stdio, :line)
 |> Stream.map(&String.trim(&1))
 |> Stream.map(&Integer.parse(&1))
--- 2018/day1b.exs
+++ 2018/day1b.exs
@@ -1,44 +1,19 @@
-# elixir day1b.exs < day1-input.txt
-defmodule Day1b do
-  def first_frequency_twice(list_of_ints) do
-    first_frequency_twice(0, list_of_ints, %{}, list_of_ints)
-  end
-
-  def first_frequency_twice(current_frequency, [hd | tl], acc, initial_list) do
-    acc = Map.put(acc, current_frequency, true)
-    f = current_frequency + hd
-
-    if Map.has_key?(acc, f) do
-      f
-    else
-      first_frequency_twice(f, tl, acc, initial_list)
-    end
-  end
-
-  def first_frequency_twice(current_frequency, [], acc, initial_list) do
-    first_frequency_twice(current_frequency, initial_list, acc, initial_list)
-  end
-end
-
-ExUnit.start()
-
-defmodule Day1bTest do
-  use ExUnit.Case
-
-  import Day1b
-
-  test "first_twice" do
-    assert first_frequency_twice([+1, -1]) == 0
-    assert first_frequency_twice([+3, +3, +4, -2, -4]) == 10
-    assert first_frequency_twice([-6, +3, +8, +5, -6]) == 5
-    assert first_frequency_twice([+7, +7, -2, -7, -4]) == 14
-  end
-end
-
-IO.stream(:stdio, :line)
+# Usage: elixir day1b.exs day1-input.txt
+System.argv()
+|> hd()
+|> File.stream!()
+|> Stream.cycle()
 |> Stream.map(&String.trim(&1))
 |> Stream.map(&Integer.parse(&1))
 |> Stream.map(&Kernel.elem(&1, 0))
-|> Enum.to_list()
-|> Day1b.first_frequency_twice()
+|> Enum.reduce_while(%{current: 0, seen: %{}}, fn x, %{current: current, seen: seen} = acc ->
+  next = current + x
+  acc = %{acc | current: next, seen: seen |> Map.put(current, true)}
+
+  if Map.has_key?(seen, next) do
+    {:halt, next}
+  else
+    {:cont, acc}
+  end
+end)
 |> IO.puts()
