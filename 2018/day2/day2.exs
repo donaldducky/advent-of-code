7c4c41cfb9da8137fad95836e60ef34518fb093c
Donald Chea <donaldducky@gmail.com>
2018-12-02 14:10:47 -0500

    Day2 - first half
    elixir day2 --test
    elixir day2 day2-input.txt

---
+++ 2018/day2/day2.exs
@@ -0,0 +1,95 @@
+defmodule Day2 do
+  def checksum(ids) do
+    {twos, threes} =
+      ids
+      |> Stream.map(&id_ok/1)
+      |> Enum.reduce({0, 0}, fn {a, b}, {x, y} -> {a + x, b + y} end)
+
+    twos * threes
+  end
+
+  def id_ok(binary) do
+    binary
+    |> count_letters
+    |> Enum.reduce({0, 0}, fn {_k, v}, {twos, threes} = acc ->
+      case v do
+        2 ->
+          {1, threes}
+
+        3 ->
+          {twos, 1}
+
+        _ ->
+          acc
+      end
+    end)
+  end
+
+  defp count_letters(binary) do
+    count_letters(binary, %{})
+  end
+
+  defp count_letters(<<c::utf8, rest::binary>>, letter_counts) do
+    n =
+      case Map.get(letter_counts, c) do
+        nil ->
+          0
+
+        val ->
+          val
+      end
+
+    count_letters(rest, Map.put(letter_counts, c, n + 1))
+  end
+
+  defp count_letters("", letter_counts) do
+    letter_counts
+  end
+end
+
+case System.argv() do
+  ["--test"] ->
+    ExUnit.start()
+
+    defmodule Day2Test do
+      use ExUnit.Case
+
+      import Day2
+
+      test "id_ok" do
+        assert id_ok("abcdef") == {0, 0}
+        assert id_ok("bababc") == {1, 1}
+        assert id_ok("abbcde") == {1, 0}
+        assert id_ok("abcccd") == {0, 1}
+        assert id_ok("aabcdd") == {1, 0}
+        assert id_ok("abcdee") == {1, 0}
+        assert id_ok("ababab") == {0, 1}
+      end
+
+      test "checksum" do
+        {:ok, io} =
+          StringIO.open("""
+          abcdef
+          bababc
+          abbcde
+          abcccd
+          aabcdd
+          abcdee
+          ababab
+          """)
+
+        assert checksum(io |> IO.stream(:line)) == 12
+      end
+    end
+
+  [input_file] ->
+    File.stream!(input_file)
+    |> Day2.checksum()
+    |> IO.puts()
+
+  _ ->
+    IO.puts("""
+    Usage:
+      elixir day2.exs input_file
+    """)
+end
