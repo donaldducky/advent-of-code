1a8347ed3764f80c4c91b541678464e8388d2aef
Donald Chea <donaldducky@gmail.com>
2018-12-11 00:11:02 -0500

    Day 9 - second half
    Had to change the approach. Seems tuples are pretty slow.

--- 2018/day9/lib/day9.ex
+++ 2018/day9/lib/day9.ex
@@ -22,28 +22,30 @@ defmodule Day9 do
       37305
 
   """
-  def marble_madness(input) do
+  def marble_madness(input), do: marble_madness(input, 1)
+
+  def marble_madness(input, multiplier) do
     [num_players, max_value] =
       input
       |> LineParser.players_points()
       |> elem(1)
 
-    board = {}
-    index = 0
+    max_value = max_value * multiplier
+
+    board = Board.new()
     value = 0
     scores = Tuple.duplicate(0, num_players)
 
     # setup
     0..max_value
-    |> Enum.reduce({board, index, scores}, fn
-      0, {board, index, scores} ->
-        {board, index} = take_turn(board, index, value)
-        {board, index, scores}
+    |> Enum.reduce({board, scores}, fn
+      0, {board, scores} ->
+        {take_turn(board, value), scores}
 
-      value, {board, index, scores} when rem(value, 23) == 0 ->
+      value, {board, scores} when rem(value, 23) == 0 ->
         # subtract 1 because tuple is zero indexed
         player_index = rem(value, num_players)
-        {board, index, points_captured} = capture_marble(board, index)
+        {board, points_captured} = capture_marble(board)
 
         player_score =
           scores
@@ -55,91 +57,42 @@ defmodule Day9 do
           scores
           |> put_elem(player_index, player_score)
 
-        {board, index, scores}
+        {board, scores}
 
-      value, {board, index, scores} ->
-        {board, index} = take_turn(board, index, value)
-        {board, index, scores}
+      value, {board, scores} ->
+        {take_turn(board, value), scores}
     end)
-    |> elem(2)
+    |> elem(1)
     |> Tuple.to_list()
     |> Enum.max()
   end
 
   @doc """
   Take a turn
-
-  ## Examples
-
-      iex> Day9.take_turn({}, 0, 0)
-      {{0}, 0}
-      iex> Day9.take_turn({0}, 0, 1)
-      {{0, 1}, 1}
-      iex> Day9.take_turn({0, 1}, 1, 2)
-      {{0, 2, 1}, 1}
-      iex> Day9.take_turn({0, 2, 1}, 1, 3)
-      {{0, 2, 1, 3}, 3}
-      iex> Day9.take_turn({0, 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15}, 15, 16)
-      {{0, 16, 8, 4, 9, 2, 10, 5, 11, 1, 12, 6, 13, 3, 14, 7, 15}, 1}
-
   """
-  def take_turn(board, index, value) do
-    index = clockwise(board, index)
-
-    board =
-      board
-      |> Tuple.insert_at(index, value)
-
-    {board, index}
+  def take_turn(board, value) do
+    board
+    |> Board.clockwise()
+    |> Board.insert(value)
   end
 
   @doc """
   Capture a marble by moving 7 counter-clockwise and removing the marble.
   Sets the index to the marble occupying the old spot.
 
-  ## Examples
-
-      iex> Day9.capture_marble({0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15}, 13)
-      {{0, 16, 8, 17, 4, 18, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15}, 6, 9}
-      iex> Day9.capture_marble({0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15}, 3)
-      {{0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 14, 7, 15}, 19, 3}
-
   """
-  def capture_marble(board, index) when index - 7 < 0,
-    do: capture_marble(board, tuple_size(board) + index)
+  def capture_marble(board) do
+    board =
+      1..7
+      |> Enum.reduce(board, fn _, board ->
+        board |> Board.counter_clockwise()
+      end)
 
-  def capture_marble(board, index) do
-    index = index - 7
-    captured = board |> elem(index)
-    board = board |> Tuple.delete_at(index)
+    points = board |> Board.head()
 
-    {board, index, captured}
-  end
+    board = board |> Board.remove()
 
-  @doc """
-  Return index, given an index after moving clockwise
-
-  ## Examples
-
-      iex> Day9.clockwise({}, 0)
-      0
-      iex> Day9.clockwise({0}, 0)
-      1
-      iex> Day9.clockwise({0, 1}, 1)
-      1
-      iex> Day9.clockwise({0, 2, 1}, 1)
-      3
-      iex> Day9.clockwise({0, 2, 1, 3}, 3)
-      1
-
-  """
-  def clockwise({}, _), do: 0
-  def clockwise({_}, _), do: 1
-  def clockwise({_, _}, _), do: 1
-  def clockwise(board, index) when index + 2 <= tuple_size(board), do: index + 2
-
-  def clockwise(board, index) do
-    rem(index + 2, board |> tuple_size)
+    {board, points}
   end
 
   def first_half() do
@@ -147,10 +100,131 @@ defmodule Day9 do
     |> marble_madness()
   end
 
+  def second_half() do
+    read_input()
+    |> marble_madness(100)
+  end
+
   @spec read_input() :: Enumerable.t()
   def read_input() do
     File.read!("input.txt")
     |> String.trim()
+  end
+end
+
+defmodule Board do
+  def new() do
+    [[], []]
+  end
+
+  @doc """
+  Get value of current position
+
+  ## Examples
+
+      iex> Board.new() |> Board.head()
+      nil
+
+  """
+  def head([[], []]), do: nil
+  def head([[head | _tail], _]), do: head
+
+  @doc """
+  Insert at current position
+
+  ## Examples
+
+      iex> Board.new() |> Board.insert(0) |> Board.head()
+      0
+      iex> Board.new() |> Board.insert(0) |> Board.insert(1) |> Board.head()
+      1
+
+  """
+  def insert([list1, list2], val) do
+    [[val | list1], list2]
+  end
+
+  @doc """
+  Remove current position
+
+  ## Examples
+
+      iex> Board.remove([[], []])
+      [[], []]
+      iex> Board.remove([[0], []])
+      [[], []]
+      iex> Board.remove([[2, 0], [1]])
+      [[1, 0], []]
+      iex> Board.remove([[3, 1, 2, 0], []])
+      [[0], [2, 1]]
+
+  """
+  def remove([[], []] = board), do: board
+
+  def remove([[_head | tail], list2]) do
+    [tail, list2]
+    |> Board.clockwise()
+  end
+
+  @doc """
+  Move current position clockwise (right) and wrap around if at bounds
+
+  ## Examples
+
+      iex> Board.clockwise([[], []])
+      [[], []]
+      iex> Board.clockwise([[0], []])
+      [[0], []]
+      iex> Board.clockwise([[1, 0], []])
+      [[0], [1]]
+      iex> Board.clockwise([[0], [1]])
+      [[1, 0], []]
+      iex> Board.clockwise([[3, 1, 2, 0], []])
+      [[0], [2, 1, 3]]
+      iex> Board.clockwise([[0], [2, 1, 3]])
+      [[2, 0], [1, 3]]
+
+  """
+  def clockwise([[], []] = board), do: board
+  def clockwise([[_], []] = board), do: board
+
+  def clockwise([list1, []]) do
+    [head | tail] = list1 |> Enum.reverse()
+    [[head], tail]
+  end
+
+  def clockwise([list1, [head2 | tail2]]) do
+    [[head2 | list1], tail2]
+  end
+
+  @doc """
+  Move current position counter-clockwise (left) and wrap around if at bounds
+
+  ## Examples
+
+      iex> Board.counter_clockwise([[], []])
+      [[], []]
+      iex> Board.counter_clockwise([[0], []])
+      [[0], []]
+      iex> Board.counter_clockwise([[1, 0], []])
+      [[0], [1]]
+      iex> Board.counter_clockwise([[0], [1]])
+      [[1, 0], []]
+      iex> Board.counter_clockwise([[3, 1, 2, 0], []])
+      [[1, 2, 0], [3]]
+      iex> Board.counter_clockwise([[0], [2, 1, 3]])
+      [[3, 1, 2, 0], []]
+
+  """
+  def counter_clockwise([[], []] = board), do: board
+  def counter_clockwise([[_], []] = board), do: board
+
+  def counter_clockwise([[head], list2]) do
+    [[head | list2] |> Enum.reverse(), []]
+  end
+
+  def counter_clockwise([[head | tail], list2]) do
+    [tail, [head | list2]]
   end
 end
 
--- 2018/day9/test/day9_test.exs
+++ 2018/day9/test/day9_test.exs
@@ -1,4 +1,5 @@
 defmodule Day9Test do
   use ExUnit.Case
   doctest Day9
+  doctest Board
 end
