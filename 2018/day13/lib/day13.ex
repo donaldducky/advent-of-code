ee0292b6f1ca0008d22b3d9218bdd22065bf9407
Donald Chea <donaldducky@gmail.com>
2018-12-14 18:31:17 -0500

    Day 13 - bit of tidying

--- 2018/day13/lib/day13.ex
+++ 2018/day13/lib/day13.ex
@@ -75,71 +75,53 @@ defmodule Day13 do
   end
 
   def step(track, carts, i) do
-    # i |> IO.inspect(label: "iteration")
-    # {w, _} = track |> Enum.to_list() |> Enum.max_by(fn {{x, _y}, _v} -> x end) |> elem(0)
-    # {_, h} = track |> Enum.to_list() |> Enum.max_by(fn {{_x, y}, _v} -> y end) |> elem(0)
-    # IO.inspect([w, h])
-
-    # 0..h
-    # |> Enum.each(fn y ->
-    #  0..w
-    #  |> Enum.each(fn x ->
-    #    c =
-    #      if Map.get(carts, {x, y}) == nil do
-    #        Map.get(track, {x, y}, ?\s)
-    #      else
-    #        "█" |> String.to_charlist()
-    #      end
-
-    #    IO.write([c])
-    #  end)
-
-    #  IO.puts("")
-    # end)
-
     carts =
       carts
-      |> Enum.reduce_while(%{}, fn {{x, y}, {direction, next_turn}}, new_carts ->
+      |> Enum.reduce_while(%{}, fn {{x, y} = pos, {direction, next_turn}}, new_carts ->
         {vx, vy} = Map.get(@dir_to_coord, direction)
-        x2 = x + vx
-        y2 = y + vy
+        next_pos = {x + vx, y + vy}
 
-        cond do
-          Map.get(carts, {x2, y2}) != nil ->
-            # there's a cart in the position we want to go to
-            # check if we're going to crash
-            #
-            # ie. this should not crash
-            # ++ -> ab -> a going straight, b going left
-            # ++    ++
-            {other_cart_direction, _next_turn} = Map.get(carts, {x2, y2})
-            {v2x, v2y} = Map.get(@dir_to_coord, other_cart_direction)
+        if crashed?(pos, next_pos, carts, new_carts) do
+          {:halt, {:crash, next_pos}}
+        else
+          track_piece = Map.get(track, next_pos)
+          heading = get_new_heading({direction, next_turn}, track_piece)
 
-            if v2x + x2 == x and v2y + y2 == y do
-              {:halt, {:crash, {x2, y2}}}
-            else
-              track_piece = Map.get(track, {x2, y2})
-              # IO.puts("Moving from #{x},#{y} to #{x2},#{y2}")
-              heading = get_new_heading({direction, next_turn}, track_piece)
-
-              {:cont, Map.put(new_carts, {x2, y2}, heading)}
-            end
-
-          Map.get(carts, {x2, y2}) == nil and Map.get(new_carts, {x2, y2}) == nil ->
-            track_piece = Map.get(track, {x2, y2})
-            # IO.puts("Moving from #{x},#{y} to #{x2},#{y2}")
-            heading = get_new_heading({direction, next_turn}, track_piece)
-
-            {:cont, Map.put(new_carts, {x2, y2}, heading)}
-
-          true ->
-            # IO.puts("Crash at #{x2},#{y2}")
-            {:halt, {:crash, {x2, y2}}}
+          {:cont, Map.put(new_carts, next_pos, heading)}
         end
       end)
 
     # carts cycle :left, :straight, :right
     step(track, carts, i + 1)
+  end
+
+  def crashed?({x, y}, {x2, y2} = next_pos, carts, new_carts) do
+    cart_at_new_position = Map.get(carts, next_pos)
+    cart_moved_to_new_position = Map.get(new_carts, next_pos)
+
+    cond do
+      cart_at_new_position != nil ->
+        # there's a cart in the position we want to go to
+        # check if we're going to crash
+        #
+        # ie. this should not crash
+        # ++  +  >v  =  +>
+        # ++     ++     +v
+        {other_cart_direction, _next_turn} = cart_at_new_position
+        {v2x, v2y} = Map.get(@dir_to_coord, other_cart_direction)
+
+        if v2x + x2 == x and v2y + y2 == y do
+          true
+        else
+          false
+        end
+
+      cart_at_new_position == nil and cart_moved_to_new_position == nil ->
+        false
+
+      true ->
+        true
+    end
   end
 
   def get_new_heading(heading, track_piece) when track_piece == ?| or track_piece == ?-,
@@ -198,6 +180,14 @@ defmodule Day13 do
     end
   end
 
+  @doc """
+
+  ## Examples
+
+      iex> Day13.first_half()
+      "26,99"
+
+  """
   def first_half() do
     File.read!("input.txt")
     |> String.trim_trailing()
