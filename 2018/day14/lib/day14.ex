6e05b5e66ffe1b28bae6b0af69851c6908f6c6b5
Donald Chea <donaldducky@gmail.com>
2018-12-14 21:27:41 -0500

    Day 14 - second half

--- 2018/day14/lib/day14.ex
+++ 2018/day14/lib/day14.ex
@@ -30,9 +30,7 @@ defmodule Day14 do
 
     next_n = 10
 
-    max_count =
-      (num_recipes + next_n)
-      |> IO.inspect(label: "max")
+    max_count = num_recipes + next_n
 
     scores = create_recipes(elves, scores, count, max_count)
 
@@ -41,16 +39,44 @@ defmodule Day14 do
     |> Enum.join()
   end
 
-  def create_recipes(elves, scores, count, max_count) do
-    # elves |> IO.inspect(label: "elves")
-    # scores |> IO.inspect(label: "scores")
-    # count |> IO.inspect(label: "count")
+  @doc """
+  ## Examples
 
+  #iex> Day14.appears_after("51589")
+  #9
+
+  iex> Day14.appears_after("01245")
+  5
+
+  #iex> Day14.appears_after("92510")
+  #18
+
+  #iex> Day14.appears_after("59414")
+  #2018
+  """
+  def appears_after(pattern) do
+    elves = [0, 1]
+
+    n = pattern |> String.length()
+
+    m = Matcher.new(pattern)
+
+    {scores, count, matcher} =
+      [3, 7]
+      |> Enum.reduce({%{}, 0, m}, fn score, {scores, i, matcher} ->
+        matcher = Matcher.next(matcher, Integer.to_string(score))
+
+        {Map.put(scores, i, score), i + 1, matcher}
+      end)
+
+    create_recipes_pattern(elves, scores, count, n, matcher)
+  end
+
+  def create_recipes(elves, scores, count, max_count) do
     {scores, count} =
       elves
       |> Enum.reduce(0, fn i, sum -> sum + (scores |> Map.get(i)) end)
       |> Integer.digits()
-      # |> IO.inspect(label: "new digits")
       |> Enum.reduce({scores, count}, fn score, {scores, i} ->
         {Map.put(scores, i, score), i + 1}
       end)
@@ -62,8 +88,6 @@ defmodule Day14 do
         rem(s + 1 + i, count)
       end)
 
-    # IO.puts("")
-
     if count < max_count do
       create_recipes(elves, scores, count, max_count)
     else
@@ -71,7 +95,72 @@ defmodule Day14 do
     end
   end
 
+  def create_recipes_pattern(elves, scores, count, n, matcher) do
+    {scores, count, matcher, found} =
+      elves
+      |> Enum.reduce(0, fn i, sum -> sum + (scores |> Map.get(i)) end)
+      |> Integer.digits()
+      |> Enum.reduce({scores, count, matcher, 0}, fn
+        score, {scores, i, matcher, 0} ->
+          matcher = Matcher.next(matcher, Integer.to_string(score))
+
+          found =
+            if Matcher.fully_matched?(matcher) do
+              i + 1 - n
+            else
+              0
+            end
+
+          {Map.put(scores, i, score), i + 1, matcher, found}
+
+        _score, {scores, i, matcher, found} ->
+          {scores, i, matcher, found}
+      end)
+
+    elves =
+      elves
+      |> Enum.map(fn i ->
+        s = Map.get(scores, i)
+        rem(s + 1 + i, count)
+      end)
+
+    if found > 0 do
+      found
+    else
+      create_recipes_pattern(elves, scores, count, n, matcher)
+    end
+  end
+
   def first_half() do
     next_10(890_691)
   end
+
+  def second_half() do
+    appears_after("890691")
+  end
+end
+
+defmodule Matcher do
+  def new(pattern) do
+    pattern = pattern |> String.codepoints()
+    [pattern, [], pattern]
+  end
+
+  def next([[next_val | rest], matches, pattern], val) do
+    if val == next_val do
+      [rest, [next_val | matches], pattern]
+    else
+      # no match, reset
+      [next_match | rest] = pattern
+
+      if next_match == val do
+        [rest, [next_match], pattern]
+      else
+        [pattern, [], pattern]
+      end
+    end
+  end
+
+  def fully_matched?([[], _, _]), do: true
+  def fully_matched?(_), do: false
 end
