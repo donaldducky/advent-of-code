dd7ec4813a56e4a5e5e79cbda22218da43595b5d
Donald Chea <donaldducky@gmail.com>
2018-12-22 19:36:08 -0500

    Day 17 - first half

--- 2018/day17/lib/day17.ex
+++ 2018/day17/lib/day17.ex
@@ -17,12 +17,6 @@ defmodule Day17 do
     "~" => :still_water,
     "|" => :flowing_water
   }
-  @directions %{
-    left: {-1, 0},
-    right: {1, 0},
-    down: {0, 1},
-    up: {0, -1}
-  }
 
   @doc """
   iex> Day17.parse_map([
@@ -74,10 +68,11 @@ defmodule Day17 do
   ...>   {501, 5} => :clay,
   ...>   {501, 6} => :clay,
   ...> })
-  %{x_min: 494, x_max: 502, y_min: 1, y_max: 7}
+  %{x_min: 494, x_max: 502, y_min: 3, y_max: 7}
   """
   def map_bounds(map) do
     map
+    |> Stream.filter(fn {_, v} -> v == :clay end)
     |> Stream.map(fn {{x, y}, _v} -> {x, y} end)
     |> Enum.reduce(%{}, fn {x, y}, bounds ->
       bounds
@@ -89,6 +84,10 @@ defmodule Day17 do
         previous_x when x > previous_x -> x
         previous_x -> previous_x
       end)
+      |> Map.update(:y_min, y, fn
+        previous_y when y < previous_y -> y
+        previous_y -> previous_y
+      end)
       |> Map.update(:y_max, y, fn
         previous_y when y > previous_y -> y
         previous_y -> previous_y
@@ -96,7 +95,6 @@ defmodule Day17 do
     end)
     |> Map.update!(:x_min, &(&1 - 1))
     |> Map.update!(:x_max, &(&1 + 1))
-    |> Map.put(:y_min, 1)
   end
 
   @doc """
@@ -114,13 +112,13 @@ defmodule Day17 do
   ...>   {501, 5} => :clay,
   ...>   {501, 6} => :clay,
   ...> })
-  "......+..\n.........\n.........\n.......#.\n.......#.\n.......#.\n.......#.\n.#######.\n"
+  ".......#.\n.......#.\n.......#.\n.......#.\n.#######.\n"
   """
   def draw_map(map) do
     bounds = map |> map_bounds()
     x_min = Map.get(bounds, :x_min)
     x_max = Map.get(bounds, :x_max)
-    y_min = 0
+    y_min = Map.get(bounds, :y_min)
     y_max = Map.get(bounds, :y_max)
 
     Enum.reduce(y_min..y_max, "", fn y, output ->
@@ -173,16 +171,14 @@ defmodule Day17 do
   end
 
   def generate_water_flow(map) do
-    bounds = map |> map_bounds()
-
-    x_min = Map.get(bounds, :x_min)
-    x_max = Map.get(bounds, :x_max)
-    y_min = 0
-    y_max = Map.get(bounds, :y_max)
+    y_max =
+      map
+      |> map_bounds()
+      |> Map.get(:y_max)
 
     {{spring_x, spring_y}, :spring} = map |> Enum.find(fn {{_x, _y}, v} -> v == :spring end)
 
-    map = step_water_flow(map, {x_min, x_max, y_min, y_max}, [{spring_x, spring_y, {0, 1}}])
+    map = step_water_flow(map, y_max, [{spring_x, spring_y}])
 
     draw_map(map)
     |> IO.puts()
@@ -190,64 +186,168 @@ defmodule Day17 do
     map
   end
 
-  def step_water_flow(map, {_x_min, _x_max, _y_min, y_max} = bounds, [{_x, y, {_vx, vy}} | rest])
-      when y + vy > y_max do
-    step_water_flow(map, bounds, rest)
+  def step_water_flow(map, y_max, [{_x, y} | rest])
+      when y + 1 > y_max do
+    step_water_flow(map, y_max, rest)
   end
 
-  def step_water_flow(map, bounds, [{x, y, {vx, vy} = direction} | rest]) do
-    x2 = x + vx
-    y2 = y + vy
+  def step_water_flow(map, _y_max, []), do: map
 
-    map =
-      case Map.get(map, {x2, y2}) do
-        nil ->
-          map = Map.put(map, {x2, y2}, :flowing_water)
-          step_water_flow(map, bounds, [{x2, y2, direction} | rest])
+  def step_water_flow(map, y_max, [{x, y} | rest]) do
+    down_tile = Map.get(map, {x, y + 1})
 
-        tile ->
-          tile |> IO.inspect(label: "unhandled tile")
-          map
+    case down_tile do
+      nil ->
+        map = Map.put(map, {x, y + 1}, :flowing_water)
+        step_water_flow(map, y_max, [{x, y + 1} | rest])
+
+      tile when tile == :clay or tile == :still_water ->
+        blocked_left? = is_blocked?(map, {x, y}, {-1, 0})
+        blocked_right? = is_blocked?(map, {x, y}, {1, 0})
+
+        case {blocked_left?, blocked_right?} do
+          {true, true} ->
+            # fill row and move back up
+            fill_row(map, {x, y})
+            |> step_water_flow(y_max, [{x, y - 1} | rest])
+
+          {true, false} ->
+            {map, {next_x, next_y}} =
+              map
+              |> fill_left({x, y}, :flowing_water)
+              |> flow_right({x, y})
+
+            step_water_flow(map, y_max, [{next_x, next_y} | rest])
+
+          {false, true} ->
+            {map, {next_x, next_y}} =
+              map
+              |> fill_right({x, y}, :flowing_water)
+              |> flow_left({x, y})
+
+            step_water_flow(map, y_max, [{next_x, next_y} | rest])
+
+          {false, false} ->
+            {map, {right_x, right_y}} =
+              map
+              |> flow_right({x, y})
+
+            {map, {left_x, left_y}} =
+              map
+              |> flow_left({x, y})
+
+            step_water_flow(map, y_max, [
+              {left_x, left_y},
+              {right_x, right_y} | rest
+            ])
+        end
+
+      :flowing_water ->
+        step_water_flow(map, y_max, rest)
+
+      tile ->
+        tile |> IO.inspect(label: "unhandled tile")
+        map
+    end
+  end
+
+  def flow(map, {x, y}, {vx, _vy}) do
+    Stream.iterate(0, &(&1 + vx))
+    |> Enum.reduce_while(map, fn i, map ->
+      map = Map.put(map, {x + i, y}, :flowing_water)
+      down_tile = Map.get(map, {x + i, y + 1})
+
+      if down_tile == :clay or down_tile == :still_water do
+        {:cont, map}
+      else
+        map = Map.put(map, {x + i, y}, :flowing_water)
+        {:halt, {map, {x + i, y}}}
       end
-
-    map
+    end)
   end
 
-  def step_water_flow(map, bounds, []) do
+  def flow_right(map, {x, y}) do
+    flow(map, {x, y}, {1, 0})
+  end
+
+  def flow_left(map, {x, y}) do
+    flow(map, {x, y}, {-1, 0})
+  end
+
+  def is_blocked?(map, {x, y}, {vx, vy} = direction) when abs(vx) == 1 and vy == 0 do
+    if Map.get(map, {x + vx, y}) == :clay do
+      true
+    else
+      # can we flow down?
+      down_tile = Map.get(map, {x + vx, y + 1})
+
+      if down_tile == :clay or down_tile == :still_water do
+        is_blocked?(map, {x + vx, y}, direction)
+      else
+        false
+      end
+    end
+  end
+
+  @doc """
+  Fill in a given direction until blocked by clay.
+  """
+  def fill(map, {x, y}, {vx, vy}, fill_with) when vy == 0 do
+    Stream.iterate(0 + vx, &(&1 + vx))
+    |> Enum.reduce_while(map, fn i, map ->
+      if Map.get(map, {x + i, y}) == :clay do
+        {:halt, map}
+      else
+        {:cont, Map.put(map, {x + i, y}, fill_with)}
+      end
+    end)
+  end
+
+  def fill_left(map, {x, y}, fill_with) do
+    fill(map, {x, y}, {-1, 0}, fill_with)
+  end
+
+  def fill_right(map, {x, y}, fill_with) do
+    fill(map, {x, y}, {1, 0}, fill_with)
+  end
+
+  def fill_row(map, {x, y}) do
     map
+    |> fill_left({x, y}, :still_water)
+    |> fill_right({x, y}, :still_water)
+    |> Map.put({x, y}, :still_water)
   end
 
   @doc """
 
   """
   def count_water_tiles(map) do
-    bounds = map |> map_bounds()
-    x_min = Map.get(bounds, :x_min)
-    x_max = Map.get(bounds, :x_max)
-    y_min = 0
-    y_max = Map.get(bounds, :y_max)
+    bounds = map_bounds(map)
+    y_min = Map.get(bounds, :y_min)
 
-    Enum.reduce(y_min..y_max, 0, fn y, sum ->
-      Enum.reduce(x_min..x_max, sum, fn x, sum ->
-        case Map.get(map, {x, y}) do
-          :still_water -> sum + 1
-          :flowing_water -> sum + 1
-          _ -> sum
-        end
-      end)
-    end)
+    map
+    |> Stream.filter(fn {{_x, y}, _tile} -> y >= y_min end)
+    |> Enum.count(fn {_, tile} -> tile == :still_water or tile == :flowing_water end)
   end
 
   @doc """
-  "iex> Day17.first_half()
-  "1
+  #iex> Day17.first_half()
+  #1
   """
   def first_half() do
     File.read!("input.txt")
     |> String.split("\n", trim: true)
     |> parse_map()
+    |> write_map("out-a.txt")
     |> generate_water_flow()
+    |> write_map("out-b.txt")
     |> count_water_tiles()
+  end
+
+  def write_map(map, filename) do
+    File.write!(filename, draw_map(map))
+
+    map
   end
 end
 
