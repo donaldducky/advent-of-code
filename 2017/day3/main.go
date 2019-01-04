6994cbd076c71628da85a950189332afb6aa107b
Donald Chea <donaldducky@gmail.com>
2019-01-03 23:22:21 -0500

    2017 Day 3 - part 2

--- 2017/day3/main.go
+++ 2017/day3/main.go
@@ -9,6 +9,7 @@ func main() {
 	in := 277678
 
 	fmt.Printf("part 1: %d\n", stepsFrom(in))
+	fmt.Printf("part 2: %d\n", firstValueLargerThan(in))
 }
 
 func stepsFrom(n int) int {
@@ -64,4 +65,67 @@ func key(x, y int) string {
 
 func manhattanDistance(x, y, x2, y2 int) int {
 	return int(math.Abs(float64(x2-x)) + math.Abs(float64(y2-y)))
+}
+
+func firstValueLargerThan(n int) int {
+	visited := make(map[string]int)
+	x := 0
+	y := 0
+	vx := 0
+	vy := 1
+	k := key(x, y)
+	visited[k] = 1
+	last := visited[k]
+	for {
+		if last > n {
+			break
+		}
+
+		vx2, vy2 := turnLeft(vx, vy)
+		x2 := x + vx2
+		y2 := y + vy2
+
+		k = key(x2, y2)
+		if _, ok := visited[k]; ok {
+			x2 = x + vx
+			y2 = y + vy
+			k = key(x2, y2)
+		} else {
+			vx = vx2
+			vy = vy2
+		}
+
+		visited[k] = sumSurrounding(x2, y2, visited)
+		last = visited[k]
+
+		x = x2
+		y = y2
+	}
+
+	return last
+}
+
+func sumSurrounding(x, y int, visited map[string]int) int {
+	sum := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+
+			sum += sumAt(x+i, y+j, visited)
+		}
+	}
+
+	return sum
+}
+
+func sumAt(x, y int, visited map[string]int) int {
+	v, ok := visited[key(x, y)]
+
+	if ok {
+		return v
+	}
+
+	return 0
 }
