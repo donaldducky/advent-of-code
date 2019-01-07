734f701e1fe235c8ed8d935bd1039244f3075f74
Donald Chea <donaldducky@gmail.com>
2019-01-07 00:23:04 -0500

    2017 day9 - part 2

--- 2017/day9/main.go
+++ 2017/day9/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
 
 func part1() int {
@@ -48,6 +49,57 @@ func part1() int {
 			case '{':
 				nesting++
 				sum += nesting
+			case '}':
+				nesting--
+				// assuming entire input is 1 group
+				// if nesting == 0 {break 2}
+			case '<':
+				garbage = true
+			}
+		}
+	}
+
+	return sum
+}
+
+func part2() int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	sum := 0
+	garbage := false
+	skip := false
+	nesting := 0
+
+	scanner := bufio.NewReader(file)
+	for {
+		c, _, err := scanner.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				panic(err)
+			}
+		}
+
+		if skip {
+			skip = false
+		} else if garbage {
+			switch c {
+			case '>':
+				garbage = false
+			case '!':
+				skip = true
+			default:
+				sum++
+			}
+		} else {
+			switch c {
+			case '{':
+				nesting++
 			case '}':
 				nesting--
 				// assuming entire input is 1 group
