6280e506b2e053ff2b36bdc826b95c386b7eeb00
Donald Chea <donaldducky@gmail.com>
2019-01-08 00:12:52 -0500

    2017 Day 10 - part 2

--- 2017/day10/main.go
+++ 2017/day10/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %s\n", part2())
 }
 
 func part1() int {
@@ -39,6 +40,51 @@ func part1() int {
 	}
 
 	return list[0] * list[1]
+}
+
+func part2() string {
+	bs, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	s := strings.TrimSpace(string(bs))
+	in := append([]byte(s), []byte{17, 31, 73, 47, 23}...)
+
+	sz := 256
+	list := make([]int, sz)
+
+	pos := 0
+	skip := 0
+	for i := 0; i < sz; i++ {
+		list[i] = i
+	}
+
+	for i := 0; i < 64; i++ {
+		for _, b := range in {
+			n := int(b)
+
+			list = reverseCircular(list, pos, n, sz)
+			pos += n + skip
+			skip++
+		}
+	}
+
+	dense := make([]int, 16)
+	for i := 0; i < 16; i++ {
+		a := i * 16
+		b := a + 16
+		dense[i] = list[a]
+		for j := a + 1; j < b; j++ {
+			dense[i] ^= list[j]
+		}
+	}
+
+	hex := make([]string, 16)
+	for i, n := range dense {
+		hex[i] = fmt.Sprintf("%02x", n)
+	}
+
+	return strings.Join(hex, "")
 }
 
 func reverseCircular(ls []int, pos, n, sz int) []int {
