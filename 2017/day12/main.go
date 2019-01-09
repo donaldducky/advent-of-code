7f45de3f41266530010aa7669de87ff6bf28ba6a
Donald Chea <donaldducky@gmail.com>
2019-01-09 00:39:38 -0500

    2017 Day 12 - part 2

--- 2017/day12/main.go
+++ 2017/day12/main.go
@@ -9,9 +9,34 @@ import (
 
 func main() {
 	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
 
 func part1() int {
+	programs := readPrograms()
+
+	seen := make(map[string]bool)
+	cnt, seen := countProgramsInGroup(programs, "0", seen)
+
+	return cnt
+}
+
+func part2() int {
+	programs := readPrograms()
+
+	cnt := 0
+	seen := make(map[string]bool)
+	for id, _ := range programs {
+		if _, ok := seen[id]; !ok {
+			cnt++
+			_, seen = countProgramsInGroup(programs, id, seen)
+		}
+	}
+
+	return cnt
+}
+
+func readPrograms() map[string][]string {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
@@ -29,12 +54,11 @@ func part1() int {
 		programs[id] = linked
 	}
 
-	return countGroups(programs)
+	return programs
 }
 
-func countGroups(pg map[string][]string) int {
-	open := []string{"0"}
-	closed := make(map[string]bool)
+func countProgramsInGroup(pg map[string][]string, s string, seen map[string]bool) (int, map[string]bool) {
+	open := []string{s}
 
 	cnt := 0
 	for {
@@ -44,19 +68,19 @@ func countGroups(pg map[string][]string) int {
 
 		id := open[0]
 		open = append(open[:0], open[1:]...)
-		if _, ok := closed[id]; ok {
+		if _, ok := seen[id]; ok {
 			continue
 		}
 
 		cnt++
-		closed[id] = true
+		seen[id] = true
 		links := pg[id]
 		for _, l := range links {
-			if _, ok := closed[l]; !ok {
+			if _, ok := seen[l]; !ok {
 				open = append(open, l)
 			}
 		}
 	}
 
-	return cnt
+	return cnt, seen
 }
