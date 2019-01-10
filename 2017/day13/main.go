e7df359bb9012e1c889dc91d494972b63970ff08
Donald Chea <donaldducky@gmail.com>
2019-01-10 00:15:17 -0500

    2017 day13 - part 1

---
+++ 2017/day13/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("Part 1: %d\n", part1())
+}
+
+func part1() int {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	layers := map[int]int{}
+	for s.Scan() {
+		var l, r int
+		_, err := fmt.Sscanf(s.Text(), "%d: %d", &l, &r)
+		if err != nil {
+			panic(err)
+		}
+
+		layers[l] = r
+	}
+
+	if err := s.Err(); err != nil {
+		panic(err)
+	}
+
+	severity := 0
+	for l, r := range layers {
+		n := 2 * (r - 1)
+		rem := l % n
+		pos := rem
+		if rem > r-1 {
+			pos = n - pos
+		}
+		if pos == 0 {
+			severity += l * r
+		}
+	}
+
+	return severity
+}
