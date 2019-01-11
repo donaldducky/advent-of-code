28ba5be42e4a7343c7389e9803cd09977c8bfa3e
Donald Chea <donaldducky@gmail.com>
2019-01-11 00:00:30 -0500

    2017 Day 15 - Part 2

--- 2017/day15/main.go
+++ 2017/day15/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
 
 func part1() int {
@@ -24,6 +25,36 @@ func part1() int {
 	return c
 }
 
+func part2() int {
+	a := 289
+	b := 629
+
+	c := 0
+
+	//n := 40000000
+	n := 5000000
+	for i := 0; i < n; i++ {
+		a = next2(a, 16807, 4)
+		b = next2(b, 48271, 8)
+		if a%65536 == b%65536 {
+			c++
+		}
+	}
+
+	return c
+}
+
 func next(v, f int) int {
 	return v * f % 2147483647
+}
+
+func next2(v, f, m int) int {
+	for {
+		v = next(v, f)
+		if v%m == 0 {
+			break
+		}
+	}
+
+	return v
 }
