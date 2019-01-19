239d8153f88773e28015b337a044f2de0eeb0611
Donald Chea <donaldducky@gmail.com>
2019-01-19 10:40:56 -0500

    2017 Day 23 - Part 2

--- 2017/day23/main.go
+++ 2017/day23/main.go
@@ -9,13 +9,70 @@ import (
 
 func main() {
 	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
 
 func part1() int {
 	instructions := getInstructions()
-	process := runProgram(instructions)
+	p := process{
+		registers: map[string]int{},
+	}
+	process := runProgram(instructions, p)
 
 	return process.mulCount
+}
+
+func part2() int {
+	// Figured out the instructions are trying to find out if numbers are
+	// composite starting from value in b and ending at value c (inclusive).
+	// Found an optimized algorithm for calculating a prime number.
+	c := 126300
+	cnt := 0
+	for b := 109300; b <= c; b += 17 {
+		if !isPrime(b) {
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+// I'm no expert in calculating primes, so I used the algorithm here:
+// https://stackoverflow.com/a/1801446
+func isPrime(n int) bool {
+	if n == 2 {
+		return true
+	}
+
+	if n == 3 {
+		return true
+	}
+
+	if n%2 == 0 {
+		return false
+	}
+
+	if n%3 == 0 {
+		return false
+	}
+
+	i := 5
+	w := 2
+
+	for {
+		if i*i > n {
+			break
+		}
+
+		if n%i == 0 {
+			return false
+		}
+
+		i += w
+		w = 6 - w
+	}
+
+	return true
 }
 
 type process struct {
@@ -35,12 +92,10 @@ func getInstructions() []string {
 	return in
 }
 
-func runProgram(in []string) process {
+func runProgram(in []string, p process) process {
 	sz := len(in)
-	p := process{
-		registers: map[string]int{},
-	}
 
+	i := 0
 	for {
 		if p.ptr < 0 || p.ptr >= sz {
 			break
@@ -63,6 +118,7 @@ func runProgram(in []string) process {
 		}
 
 		p.incPtr(j)
+		i++
 	}
 
 	return p
@@ -91,4 +147,14 @@ func (p *process) set(s string, v int) {
 
 func (p *process) incPtr(v int) {
 	p.ptr = p.ptr + v
+}
+
+func (p process) printRegisters(cmd string) {
+	registers := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+
+	rs := []string{}
+	for _, r := range registers {
+		rs = append(rs, fmt.Sprintf("%s=%d", r, p.val(r)))
+	}
+	fmt.Printf("ip=%d %s %s\n", p.ptr, cmd, strings.Join(rs, " "))
 }
