570d7d5d01500c8a0304d27d3731b0153dfcd598
Don Chea <donaldducky@gmail.com>
2019-12-01 11:02:52 -0500

    2019 Day 1 - part 2

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -3,15 +3,34 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    let mut sum: u32 = 0;
+    let mut sum1: u32 = 0;
+    let mut sum2: u32 = 0;
     for line in input.lines() {
         let mass: u32 = line.parse().unwrap();
-        sum += fuel_required(mass);
+        sum1 += fuel_required(mass);
+        sum2 += total_fuel_required(mass, 0);
     }
 
-    println!("Fuel required: {}", sum);
+    println!("Fuel required for part 1: {}", sum1);
+    println!("Fuel required for part 2: {}", sum2);
 }
 
 fn fuel_required(mass: u32) -> u32 {
-    (mass / 3) - 2
+    let fuel: i32 = (mass as i32 / 3) - 2;
+
+    if fuel < 0 {
+        0
+    } else {
+        fuel as u32
+    }
+}
+
+fn total_fuel_required(mass: u32, sum: u32) -> u32 {
+    let fuel = fuel_required(mass);
+
+    if fuel > 0 {
+        total_fuel_required(fuel, sum + fuel)
+    } else {
+        sum + fuel
+    }
 }
