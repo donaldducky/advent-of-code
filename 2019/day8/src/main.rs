de5a628ff50f1c0e6098c2615c34411b4a53b8e1
Don Chea <donaldducky@gmail.com>
2019-12-14 10:38:28 -0500

    2019 Day 8 - Part 1

---
+++ 2019/day8/src/main.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let pixels: Vec<u8> = input.trim()
+        .chars()
+        .map(|x| x.to_digit(10).unwrap() as u8)
+        .collect();
+
+    println!("Part 1: {}", calculate_checksum(&pixels, 25, 6));
+}
+
+fn calculate_checksum(pixels: &Vec<u8>, width: u8, height: u8) -> u32 {
+    let pixels_per_layer: usize = (width * height) as usize;
+    let layers: Vec<&[u8]> = pixels.chunks(pixels_per_layer).collect();
+
+    let min_layer = layers.iter()
+        .map(|layer| {
+            let mut digits_map: HashMap<u8, u8> = HashMap::new();
+            layer.iter().for_each(|n| {
+                let n_count = digits_map.entry(*n).or_insert(0);
+                *n_count += 1;
+            });
+            digits_map
+        })
+        .min_by(|x, y| x[&0].cmp(&y[&0])).unwrap();
+
+    (min_layer[&1] as u32) * (min_layer[&2] as u32)
+}
