ef9a50711aaf018d9ba0973d1479fd69c17b0e23
Don Chea <donaldducky@gmail.com>
2020-07-08 21:14:30 -0400

    Almost there, one edge case to deal with

--- 2019/day14/src/main.rs
+++ 2019/day14/src/main.rs
@@ -1,19 +1,29 @@
+use itertools::Itertools;
+use std::collections::HashMap;
 use std::fmt;
 use std::fs;
-use itertools::Itertools;
 
+#[derive(Debug,Eq,PartialEq,Hash,Clone)]
 struct Chemical {
     id: String,
-    quantity: u8
+    quantity: u32
 }
 
+#[derive(Debug,Eq,PartialEq,Hash,Clone)]
 struct Reaction {
     input_chemicals: Vec<Chemical>,
     output_chemical: Chemical,
 }
 
+#[derive(Debug)]
+struct Expand {
+    to_expand: Vec<Chemical>,
+    extra: HashMap<String, u32>,
+    ores: u32
+}
+
 impl Chemical {
-    fn new(id: String, quantity: u8) -> Chemical {
+    fn new(id: String, quantity: u32) -> Chemical {
         Chemical {
             id: id,
             quantity: quantity
@@ -38,7 +48,17 @@ impl Reaction {
 
 impl fmt::Display for Reaction {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}\n=> {}", self.input_chemicals.iter().format(" "), self.output_chemical)
+        write!(f, "{} => {}", self.input_chemicals.iter().format(" "), self.output_chemical)
+    }
+}
+
+impl Expand {
+    fn new(inputs: Vec<Chemical>) -> Expand {
+        Expand {
+            to_expand: inputs,
+            extra: HashMap::new(),
+            ores: 0
+        }
     }
 }
 
@@ -79,18 +99,113 @@ fn parse_chemical(chemical: &str) -> Chemical {
         panic!("Could not parse chemical from {}", chemical)
     }
 
-    let qty = parts[0].parse::<u8>().unwrap();
+    let qty = parts[0].parse::<u32>().unwrap();
     let id = parts[1];
 
     Chemical::new(id.to_string(), qty)
 }
 
 fn calculate_ore_required(input: String) -> u32 {
-    let _reactions = parse_reactions(input);
+    let reactions = parse_reactions(input);
     //println!("--- Reactions ---\n{}", reactions.iter().format("\n"));
 
-    1
+    // find input OREs
+    let ores = find_ores(reactions.clone());
+    println!("ORES: {}", ores.iter().format("\n"));
+
+    // convert to HashMap so we can easily look up reactions by output
+    let reactions_by_output: HashMap<String, Reaction> = reactions.into_iter()
+        .fold(HashMap::new(), |mut acc, r| {
+            acc.insert(r.output_chemical.id.to_string(), r);
+            acc
+        });
+
+    //println!("MAP: {:#?}", reactions_by_output);
+
+    let fuel = match reactions_by_output.get("FUEL") {
+        Some(r) => r,
+        None => panic!("Could not find FUEL")
+    };
+    println!("FUEL requirements: {}", fuel.input_chemicals.iter().format(", "));
+
+    let mut expand = Expand::new(fuel.input_chemicals.clone());
+    println!("{:#?}", expand);
+
+    let max_iters: u32 = 5;
+    let infinite: bool = true;
+    let mut i: u32 = 0;
+    while expand.to_expand.len() > 0 {
+        if !infinite && i >= max_iters {
+            break;
+        }
+        i = i + 1;
+
+        println!("--- Iteration #{}: {}", i, expand.to_expand.iter().format(", "));
+
+        let c = match expand.to_expand.pop() {
+            Some(c) => c,
+            None => panic!("Could not find item to expand")
+        };
+
+        let r = match reactions_by_output.get(&c.id) {
+            Some(r) => r,
+            None => panic!("Could not find reactions for chemical {}", c)
+        };
+        println!("[Expand] {}: {}", c, r);
+        if r.input_chemicals.len() == 1 && r.input_chemicals[0].id == "ORE" {
+            // find out how many "extras" we have
+            let extras = match expand.extra.get(&c.id) {
+                Some(n) => *n,
+                None => 0
+            };
+            // figure out the actual quantity we need
+            if extras > c.quantity {
+                // we have more extra than we need, no need to create any reactions
+                let leftover = extras - c.quantity;
+
+                expand.extra.insert(c.id, leftover);
+            } else {
+                let ores_per_reaction = r.input_chemicals[0].quantity;
+                let quantity_per_reaction = r.output_chemical.quantity;
+                let quantity_needed = c.quantity - extras;
+                let reactions_needed = (quantity_needed as f64 / quantity_per_reaction as f64).ceil() as u32;
+                println!("  extra {} = {}", &c.id, extras);
+                println!("  need {} {} = {} reactions", quantity_needed, &c.id, reactions_needed);
+
+                let ores_consumed = ores_per_reaction * reactions_needed;
+                let quantity_created = quantity_per_reaction * reactions_needed;
+                let leftover = quantity_created - quantity_needed;
+                println!("  \x1b[32m{} ores\x1b[0m used to make \x1b[32m{} {}\x1b[0m with \x1b[33m{}\x1b[0m leftover", ores_consumed, quantity_created, r.output_chemical.id, leftover);
+
+                expand.extra.insert(c.id, leftover);
+                expand.ores += ores_consumed;
+            }
+        } else {
+            let quantity_needed = c.quantity;
+            let quantity_produced = r.output_chemical.quantity;
+            let multiplier = (quantity_needed as f64 / quantity_produced as f64).ceil() as u32;
+
+            let mut chemicals_to_append = r.input_chemicals.clone();
+            chemicals_to_append.iter_mut()
+                .for_each(|c| c.quantity *= multiplier);
+
+            println!("  \x1b[33mfound {}\x1b[0m \x1b[34mx{}\x1b[0m = \x1b[33m{}\x1b[0m", r.input_chemicals.iter().format(", "), multiplier, chemicals_to_append.clone().iter().format(", "));
+
+            expand.to_expand.append(&mut chemicals_to_append);
+        }
+
+        println!("");
+    }
+
+    expand.ores
 }
+
+fn find_ores(reactions: Vec<Reaction>) -> Vec<Reaction> {
+    reactions.into_iter()
+        .filter(|r| r.input_chemicals.len() == 1 && r.input_chemicals[0].id == "ORE")
+        .collect()
+}
+
 
 #[cfg(test)]
 mod tests {
