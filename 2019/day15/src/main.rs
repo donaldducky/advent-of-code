8719efb39dfeaa06589dac332b62175c487ff9ce
Don Chea <donaldducky@gmail.com>
2020-07-10 13:32:36 -0400

    2019 Day 15 part 2
    Quick copy/paste job.

--- 2019/day15/src/main.rs
+++ 2019/day15/src/main.rs
@@ -77,6 +77,7 @@ impl Point {
 }
 
 const WALL_TILE: i64 = -1;
+const OXYGEN_TILE: i64 = -2;
 #[derive(Debug)]
 struct DroidState {
     position: Point,
@@ -113,11 +114,12 @@ impl DroidState {
         //println!( "Possible positions to move {:?} path {:?}", positions, self.path);
         match positions.len() {
             0 => {
-                // we cannot move anywhere, let's try to backtrack
-                self.path
-                    .pop()
-                    .expect("We ran out of places to move and we cannot backtrack")
-                    .reverse()
+                if self.path.len() > 0 {
+                    // we cannot move anywhere, let's try to backtrack
+                    self.path.pop().unwrap().reverse()
+                } else {
+                    Direction::None
+                }
             }
             _ => positions.pop().unwrap(),
         }
@@ -133,17 +135,18 @@ impl DroidState {
 
     fn draw(&self) {
         print!("\x1b[2J");
-        let pad = 20;
+        let pad = 40;
         for y in ((self.position.y - pad)..(self.position.y + pad)).rev() {
             for x in (self.position.x - pad)..(self.position.x + pad) {
                 if x == self.position.x && y == self.position.y {
-                    print!("d");
+                    print!("\x1b[32mD\x1b[0m");
                 } else if x == 0 && y == 0 {
-                    print!("X");
+                    print!("\x1b[31mX\x1b[0m");
                 } else {
                     match self.closed.get(&Point::new(x, y)) {
                         Some(v) => match *v {
-                            WALL_TILE => print!("@"),
+                            WALL_TILE => print!("#"),
+                            OXYGEN_TILE => print!("\x1b[44mO\x1b[0m"),
                             _other => print!("."),
                         },
                         None => print!(" "),
@@ -162,6 +165,7 @@ fn main() {
     let program = intcode::read_program(filename);
 
     println!("Part 1: {}", part1(program.clone()));
+    println!("Part 2: {}", part2(program.clone()));
 }
 
 fn part1(program: Vec<i128>) -> usize {
@@ -205,15 +209,18 @@ fn part1(program: Vec<i128>) -> usize {
                                 //state.draw();
                                 //println!("\x1b[31mFound Oxygen!\x1b[0m @{:?}", state.position);
                                 //println!("Took {} steps", state.path.len());
+                                state.visit(state.position, OXYGEN_TILE);
                                 break;
                             }
                             _ => panic!("Unknown output response {}", out),
                         }
 
-                        if i > max_i - 10 {
+                        if i > max_i - 100 {
                             println!("<- {:<10} {:?}", out, state);
                         }
-                        //state.draw();
+                        if i % 10 == 0 {
+                            //state.draw();
+                        }
                     }
                     Cmd::RequestInput() => {
                         let d = state.get_next_direction();
@@ -238,6 +245,127 @@ fn part1(program: Vec<i128>) -> usize {
             }
 
             state.path.len()
+        })
+        .unwrap();
+
+    cpu_handle.join().unwrap();
+    robot_handle.join().unwrap()
+}
+
+fn part2(program: Vec<i128>) -> usize {
+    let mut cpu = CPU::new("day15".to_string(), program);
+
+    let (cpu_tx, robot_rx) = mpsc::channel();
+    let (robot_tx, cpu_rx) = mpsc::channel();
+
+    let cpu_handle = thread::Builder::new()
+        .name("cpu".to_string())
+        .spawn(move || {
+            cpu.run(&cpu_tx, &cpu_rx);
+        })
+        .unwrap();
+
+    let robot_handle = thread::Builder::new()
+        .name("robot".to_string())
+        .spawn(move || {
+            let mut i = 0;
+
+            let mut state = DroidState::new();
+            // set dummy initial value
+            let mut oxygen_tank = Point::new(0, 0);
+
+            loop {
+                i += 1;
+
+                match robot_rx.recv().unwrap() {
+                    Cmd::Halt() => break,
+                    Cmd::Output(out) => {
+                        match out {
+                            0 => {
+                                state.visit(state.position, WALL_TILE);
+                                state.position =
+                                    state.position.add(state.direction.reverse().to_point());
+                                state.path.pop();
+                            }
+                            1 => {
+                                state.visit(state.position, state.path.len() as i64);
+                            }
+                            2 => {
+                                //state.draw();
+                                //println!("\x1b[31mFound Oxygen!\x1b[0m @{:?}", state.position);
+                                //println!("Took {} steps", state.path.len());
+                                oxygen_tank = state.position;
+                                state.visit(state.position, OXYGEN_TILE);
+                                //break;
+                            }
+                            _ => panic!("Unknown output response {}", out),
+                        }
+
+                        if i % 10 == 0 {
+                            //state.draw();
+                        }
+                    }
+                    Cmd::RequestInput() => {
+                        match state.get_next_direction() {
+                            Direction::None => {
+                                // We are done exploring
+                                break;
+                            }
+                            d => {
+                                state.direction = d;
+                                state.position = state.position.add(state.direction.to_point());
+                                if !state.is_visited(state.position) {
+                                    state.path.push(d);
+                                }
+
+                                robot_tx.send(Cmd::Input(d as i128)).unwrap();
+                            }
+                        }
+                    }
+                    _ => panic!("Unhandled command"),
+                }
+            }
+
+            //state.draw();
+            //println!("Exploring complete! Oxygen tank at {:?}", oxygen_tank);
+
+            // ok let's flood fill the oxygen
+            let mut j = 0;
+            let mut to_fill: HashMap<Point, i64> = state
+                .closed
+                .clone()
+                .iter()
+                .filter(|(_p, v)| **v != WALL_TILE)
+                .fold(HashMap::new(), |mut acc, (p, v)| {
+                    acc.insert(p.clone(), *v);
+                    acc
+                });
+            let mut open: Vec<Point> = vec![oxygen_tank];
+            while open.len() > 0 {
+                let mut next: Vec<Point> = vec![];
+
+                while open.len() > 0 {
+                    let current = open.pop().unwrap();
+                    //println!("Current: {:?}", current);
+                    let filtered: Vec<Point> = current
+                        .expand()
+                        .iter()
+                        .filter(|(p, _d)| to_fill.contains_key(p))
+                        .map(|(p, _d)| p.clone())
+                        .collect();
+                    filtered.iter().for_each(|p| {
+                        to_fill.remove(p);
+                    });
+                    next.extend(filtered);
+
+                    //println!("Expand: {:?}", next);
+                }
+                open = next;
+
+                j += 1;
+            }
+
+            j - 1
         })
         .unwrap();
 
