e3c4f67a278bec897e264a4ecede8fd3a1f7f6cd
Don Chea <donaldducky@gmail.com>
2020-07-11 21:13:01 -0400

    2019 Day 16 - Part 2

--- 2019/day16/src/main.rs
+++ 2019/day16/src/main.rs
@@ -47,8 +47,69 @@ fn part1(input: &String, num_phases: u32, num_digits: usize) -> String {
         .join("")
 }
 
-fn part2(_input: &String) -> i64 {
-    0
+fn part2(input: &String) -> String {
+    let _base_pattern: [i64; 4] = [0, 1, 0, -1];
+    let ints = parse_input(&input);
+    let size: usize = ints.len() * 10000;
+    let mut digits: Vec<i64> = vec![0; size];
+
+    let offset = &ints[0..7]
+        .iter()
+        .map(|i| i.to_string())
+        .collect::<Vec<String>>()
+        .join("")
+        .parse::<usize>()
+        .unwrap();
+
+    // TODO: Is there a faster way to build this?
+    // Looking for a way to multiply an array 10000 times was hard.
+    // Either way, it doesn't take too long that it'll make the program too slow.
+    let mut it = ints.iter().cycle();
+    for i in 0..size {
+        digits[i] = *it.next().unwrap();
+    }
+
+    // println!("ints = {:?}", ints);
+    // println!("ints.len() = {}", ints.len());
+    // println!("x10000 = {}", size);
+    // println!("offset = {:?}", offset);
+
+    // The way the base pattern is applied turns this into a triangular matrix.
+    // 1 2 3 4 5 6 7 8 9 10        1  0 -1  0  1  0 -1  0  1  0
+    //   2 3 4 5 6 7 8 9 10        0  1  1  0  0 -1 -1  0  0 -1
+    //     3 4 5 6 7 8 9 10        0  0  1  1  1  0  0  0 -1 -1
+    //       4 5 6 7 8 9 10        0  0  0  1  1  1  1  0  0  0
+    //         5 6 7 8 9 10   =>   0  0  0  0  1  1  1  1  1  0
+    //           6 7 8 9 10        0  0  0  0  0  1  1  1  1  1
+    //             7 8 9 10        0  0  0  0  0  0  1  1  1  1
+    //               8 9 10        0  0  0  0  0  0  0  1  1  1
+    //                 9 10        0  0  0  0  0  0  0  0  1  1
+    //                   10        0  0  0  0  0  0  0  0  0  1
+    //
+    // The offset is past the halfway point.
+    // length = 6500000 and offset = 5979067
+    // The only part of the base pattern we need to multiply by is 1.
+    // Because of this, we can just sum the digits.
+
+    for _i in 0..100 {
+        // This is the summation of the entire row starting at the offset.
+        // Each iteration can then do one subtraction, rather than summing all
+        // the values again.
+        let mut row_sum: i64 = digits.iter().skip(*offset).sum();
+        digits.iter_mut().skip(*offset).for_each(|d| {
+            let temp_sum = row_sum;
+            row_sum = row_sum - *d;
+            *d = (temp_sum % 10).abs();
+        });
+    }
+
+    digits
+        .iter()
+        .skip(*offset)
+        .take(8)
+        .map(|i| i.to_string())
+        .collect::<Vec<String>>()
+        .join("")
 }
 
 fn parse_input(input: &String) -> Vec<i64> {
