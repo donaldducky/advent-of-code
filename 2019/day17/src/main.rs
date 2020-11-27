f0ad227cb3860aaab860e349a87340518688559c
Don Chea <donaldducky@gmail.com>
2020-11-27 15:44:03 -0500

    2019 Day 17 - Part 2

--- 2019/day17/src/main.rs
+++ 2019/day17/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     let program = intcode::read_program(filename);
 
     println!("Part 1: {}", part1(&program));
+    println!("Part 2: {}", part2(&program));
 }
 
 fn part1(program: &Vec<i128>) -> u64 {
@@ -45,7 +46,7 @@ fn part1(program: &Vec<i128>) -> u64 {
                                 x += 1;
                             }
                         };
-                        //print!("{}", (out as u8) as char);
+                        print!("{}", (out as u8) as char);
                     }
                     Cmd::RequestInput() => panic!("RequestInput not supported yet."),
                     _ => panic!("Unhandled command"),
@@ -79,6 +80,72 @@ fn part1(program: &Vec<i128>) -> u64 {
                 })
                 .map(|((x, y), _c)| x * y)
                 .sum()
+        })
+        .unwrap();
+
+    cpu_handle.join().unwrap();
+    control_handle.join().unwrap()
+}
+
+fn part2(program: &Vec<i128>) -> u64 {
+    // override to wake up robot
+    let mut new_program = program.clone();
+    new_program[0] = 2;
+    let mut cpu = CPU::new("day17".to_string(), new_program);
+
+    let (cpu_tx, control_rx) = mpsc::channel();
+    let (control_tx, cpu_rx) = mpsc::channel();
+
+    let cpu_handle = thread::Builder::new()
+        .name("cpu".to_string())
+        .spawn(move || {
+            cpu.run(&cpu_tx, &cpu_rx);
+        })
+        .unwrap();
+
+    let control_handle = thread::Builder::new()
+        .name("control".to_string())
+        .spawn(move || {
+            let mut screen: HashMap<(u64, u64), char> = HashMap::new();
+            let mut input_started = false;
+            let mut x = 0;
+            let mut y = 0;
+            let mut inputs =
+                b"A,B,A,C,A,C,B,C,C,B\nL,4,L,4,L,10,R,4\nR,4,L,4,L,4,R,8,R,10\nR,4,L,10,R,10\nn\n"
+                    .to_vec();
+            // reverse inputs so we can pop
+            inputs.reverse();
+            let mut output = 0;
+
+            loop {
+                match control_rx.recv().unwrap() {
+                    Cmd::Halt() => break,
+                    Cmd::Output(out) => {
+                        if !input_started {
+                            match (out as u8) as char {
+                                '\n' => {
+                                    y += 1;
+                                    x = 0;
+                                }
+                                c => {
+                                    screen.insert((x, y), c);
+                                    x += 1;
+                                }
+                            };
+                        } else {
+                            output = out;
+                        }
+                    }
+                    Cmd::RequestInput() => {
+                        input_started = true;
+                        let val = inputs.pop().unwrap();
+                        control_tx.send(Cmd::Input(val as i128)).unwrap();
+                    }
+                    _ => panic!("Unhandled command"),
+                }
+            }
+
+            output as u64
         })
         .unwrap();
 
