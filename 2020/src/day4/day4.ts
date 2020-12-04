019a772c4820d6b0c04f04b93cdbd889f693afeb
Don Chea <donaldducky@gmail.com>
2020-12-04 08:41:15 -0500

    Some small simplifications to day4
    - de-duplicate passport building code
    - split input on 2 newlines so each line is grouped by passport, this
    also prevents having to compensate for an off by 1 when processing them
    - learned about Object.fromEntries to build an object (requires ES2019)
    - use unary + to cast to an int rather than parseInt (less chars)

--- 2020/src/day4/day4.ts
+++ 2020/src/day4/day4.ts
@@ -1,103 +1,50 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const lines = fs
+const passports = fs
   .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
   .toString()
   .trim()
-  .split('\n');
+  .split('\n\n')
+  .map(line =>
+    // array of key/value pairs
+    // [['byr', 2002], ['iyr', 2020], ...]
+    line.split(/[\n ]/).map(field => field.split(':'))
+  )
+  .map(Object.fromEntries);
 
-console.log('Part 1:', part1(lines));
-console.log('Part 2:', part2(lines));
-
-function part1(lines): number {
-  let currentPassport = {};
-
-  let validPassports = lines.reduce((count, line) => {
-    if (line == '') {
-      if (isValid(currentPassport)) {
-        count++;
-      }
-      currentPassport = {};
-    } else {
-      currentPassport = line.split(' ').reduce((passport, l) => {
-        const [k, v] = l.split(':');
-
-        passport[k] = v;
-
-        return passport;
-      }, currentPassport);
-    }
-
-    return count;
-  }, 0);
-
-  if (isValid(currentPassport)) {
-    validPassports++;
-  }
-
-  return validPassports;
-}
-
-function part2(lines) {
-  let currentPassport = {};
-
-  let validPassports = lines.reduce((count, line) => {
-    if (line == '') {
-      if (isValid2(currentPassport)) {
-        count++;
-      }
-      currentPassport = {};
-    } else {
-      currentPassport = line.split(' ').reduce((passport, l) => {
-        const [k, v] = l.split(':');
-
-        passport[k] = v;
-
-        return passport;
-      }, currentPassport);
-    }
-
-    return count;
-  }, 0);
-
-  if (isValid2(currentPassport)) {
-    validPassports++;
-  }
-
-  return validPassports;
-}
+console.log('Part 1:', passports.filter(isValid).length);
+console.log('Part 2:', passports.filter(isValid).filter(isValid2).length);
 
 function isValid(passport) {
-  return ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'].every(k =>
-    passport.hasOwnProperty(k)
+  return ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'].every(
+    k =>
+      // had to rewrite this for eslint because of no-prototype-builtins
+      // https://eslint.org/docs/rules/no-prototype-builtins
+      // passport.hasOwnProperty(k)
+      //passport[k] ?? false
+      //Object.prototype.hasOwnProperty.call(passport, k)
+      k in passport
   );
 }
 
 function isValid2(passport) {
-  if (!isValid(passport)) {
+  if (!(+passport.byr >= 1920 && +passport.byr <= 2002)) {
     return false;
   }
-
-  const byr = parseInt(passport.byr, 10);
-  if (!(byr >= 1920 && byr <= 2002)) {
+  if (!(+passport.iyr >= 2010 && +passport.iyr <= 2020)) {
     return false;
   }
-  const iyr = parseInt(passport.iyr, 10);
-  if (!(iyr >= 2010 && iyr <= 2020)) {
-    return false;
-  }
-  const eyr = parseInt(passport.eyr, 10);
-  if (!(eyr >= 2020 && eyr <= 2030)) {
+  if (!(+passport.eyr >= 2020 && +passport.eyr <= 2030)) {
     return false;
   }
   if (passport.hgt.match(/\d+cm/)) {
-    const hgt = parseInt(passport.hgt.split('cm')[0], 10);
+    const hgt = +passport.hgt.split('cm')[0];
     if (!(hgt >= 150 && hgt <= 193)) {
       return false;
     }
   } else if (passport.hgt.match(/\d+in/)) {
-    const hgt = parseInt(passport.hgt.split('in')[0], 10);
+    const hgt = +passport.hgt.split('in')[0];
     if (!(hgt >= 59 && hgt <= 76)) {
       return false;
     }
