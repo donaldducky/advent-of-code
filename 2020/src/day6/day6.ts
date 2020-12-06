ee3774b3c4aaa6bb69be2ff56b7d26f71b21149d
Don Chea <donaldducky@gmail.com>
2020-12-06 00:32:20 -0500

    2020 Day 6

---
+++ 2020/src/day6/day6.ts
@@ -0,0 +1,55 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const lines = fs
+  .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+  .toString()
+  .trim()
+  .split('\n\n');
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+console.log('Part 1:', part1(lines));
+console.log('Part 2:', part2(lines));
+
+function part1(lines): number {
+  return lines
+    .map(line => {
+      let l = line.split(/[\n]/).flatMap(a => a.split(''));
+      l.sort();
+      l = l.filter(onlyUnique);
+
+      return l.length;
+    })
+    .reduce((sum, c) => sum + c, 0);
+}
+
+function part2(lines): number {
+  return lines.reduce((acc, group) => {
+    let answers = group.split('\n').map(a => a.split(''));
+    let n = answers.length;
+
+    let seen = {};
+    answers.forEach(a => {
+      a.forEach(ans => {
+        if (ans in seen) {
+          seen[ans]++;
+        } else {
+          seen[ans] = 1;
+        }
+      });
+    });
+
+    return (
+      Object.keys(seen).reduce((acc, k) => {
+        if (seen[k] === n) {
+          return acc + 1;
+        } else {
+          return acc;
+        }
+      }, 0) + acc
+    );
+  }, 0);
+}
