979fa03e0356deb2f55611a21291b50b4b13afc4
Don Chea <donaldducky@gmail.com>
2020-12-07 01:38:11 -0500

    Bit of clean up

--- 2020/src/day7/day7.ts
+++ 2020/src/day7/day7.ts
@@ -1,54 +1,34 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+let file = 'input.txt';
+//file = 'sample.txt';
+//file = 'sample2.txt';
+
 const input = fs
-  //.readFileSync(path.join(__dirname, 'sample.txt'), 'utf8')
-  //.readFileSync(path.join(__dirname, 'sample2.txt'), 'utf8')
-  .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+  .readFileSync(path.join(__dirname, file), 'utf8')
   .toString()
   .trim();
 console.log('Part 1:', part1(input));
 console.log('Part 2:', part2(input));
 
 function part1(input) {
-  let reverse = {};
-  let bags = input
-    .split('\n')
-    .map(line => line.split(/contain/))
-    .map(([bag, contains]) => [
-      bag.trim().replace(/ bags$/, ''),
-      contains
-        .trim()
-        .split(', ')
-        .filter(x => !x.match(/no other/))
-        .map(x => {
-          const a = x
-            .trim()
-            .replace(/ bags| bag/, '')
-            .replace(/\./, '')
-            .split(' ');
+  let reverse = parse(input).reduce((reverse, [bag, contains]) => {
+    contains.forEach(([count, id]) => {
+      if (!(id in reverse)) {
+        reverse[id] = [];
+      }
+      reverse[id].push([bag, count]);
+    });
+    return reverse;
+  }, {});
 
-          return [a.shift(), a.join(' ')];
-        }),
-    ])
-    .filter(([bag, contains]) => contains.length > 0)
-    .reduce((bags, [bag, contains]) => {
-      bags[bag] = contains;
-      contains.forEach(([count, id]) => {
-        if (!(id in reverse)) {
-          reverse[id] = [];
-        }
-        reverse[id].push([bag, count]);
-        //console.log(bag, id);
-      });
-      return bags;
-    }, {});
-  //console.log('reverse', reverse);
   let colors = new Set();
-  let open = reverse['shiny gold'];
+  // initialize to empty in case no bags can carry shiny gold (ie. sample2.txt)
+  let open = reverse['shiny gold'] ?? [];
   delete reverse['shiny gold'];
   while (open.length) {
-    const [c /*count*/] = open.pop();
+    const [c] = open.pop();
     colors.add(c);
     if (c in reverse) {
       open = open.concat(reverse[c]);
@@ -56,63 +36,37 @@ function part1(input) {
     delete reverse[c];
   }
 
-  //console.log(colors);
-
   return colors.size;
 }
 
 function part2(input) {
-  let reverse = {};
-  let bags = input
-    .split('\n')
-    .map(line => line.split(/contain/))
-    .map(([bag, contains]) => {
-      const b = bag.trim().replace(/ bags$/, '');
-      let c;
-      if (contains.match(/no other bags/)) {
-        c = [];
-      } else {
-        c = contains
-          .trim()
-          .split(', ')
-          .map(x => {
-            const a = x
-              .trim()
-              .replace(/ bags| bag/, '')
-              .replace(/\./, '')
-              .split(' ');
-
-            const r = [+a.shift(), a.join(' ')];
-            //console.log(r);
-            return r;
-          });
-      }
-
-      return [b, c];
-    })
-    .reduce((bags, [bag, contains]) => {
-      bags[bag] = contains;
-      contains.forEach(([count, id]) => {
-        if (!(id in reverse)) {
-          reverse[id] = [];
-        }
-        reverse[id].push([bag, count]);
-        //console.log(bag, id);
-      });
-      return bags;
-    }, {});
-  //console.log(bags);
+  let bags = parse(input).reduce((bags, [bag, contains]) => {
+    bags[bag] = contains;
+    return bags;
+  }, {});
   let count = 0;
   let open = [['shiny gold', 1]];
   while (open.length) {
     let [id, multiplier] = open.pop();
-    let contains = bags[id];
     count += +multiplier;
-    //console.log(id, multiplier, contains);
-    contains.forEach(([count, id]) => {
+    bags[id].forEach(([count, id]) => {
       open.push([id, count * +multiplier]);
     });
   }
 
   return count - 1;
+}
+
+function parse(input) {
+  return input
+    .replace(/ bags?\.?/g, '')
+    .replace(/no other/g, '')
+    .split('\n')
+    .map(x => x.split(/ contain /))
+    .map(x => [
+      x[0],
+      x[1] != ''
+        ? x[1].split(', ').map(x => x.match(/(\d) (.*)/).slice(1))
+        : [],
+    ]);
 }
