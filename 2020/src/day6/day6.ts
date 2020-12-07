6768aa5fb3b76b4e1317318b3bf4a727cd006b68
Don Chea <donaldducky@gmail.com>
2020-12-06 22:06:53 -0500

    Simplify 2020 Day 6

--- 2020/src/day6/day6.ts
+++ 2020/src/day6/day6.ts
@@ -1,47 +1,32 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const lines = fs
+const input = fs
   .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
   .toString()
-  .trim()
-  .split('\n\n');
+  .trim();
+console.log('Part 1:', part1(input));
+console.log('Part 2:', part2(input));
 
-console.log('Part 1:', part1(lines));
-console.log('Part 2:', part2(lines));
-
-function part1(lines): number {
-  return lines
-    .map(line => {
-      return new Set(line.split(/[\n]/).flatMap(a => a.split(''))).size;
-    })
-    .reduce((sum, c) => sum + c, 0);
+function part1(input) {
+  return (
+    input
+      .split('\n\n')
+      //.map(g => new Set(g.split(/\n/).join('').split('')).size)
+      .map(g => new Set(g.split(/\n/).flatMap(a => a.split(''))).size)
+      .reduce((a, b) => a + b)
+  );
 }
 
-function part2(lines): number {
-  return lines.reduce((acc, group) => {
-    let answers = group.split('\n').map(a => a.split(''));
-    let n = answers.length;
-
-    let seen = {};
-    answers.forEach(a => {
-      a.forEach(ans => {
-        if (ans in seen) {
-          seen[ans]++;
-        } else {
-          seen[ans] = 1;
-        }
-      });
-    });
-
-    return (
-      Object.keys(seen).reduce((acc, k) => {
-        if (seen[k] === n) {
-          return acc + 1;
-        } else {
-          return acc;
-        }
-      }, 0) + acc
-    );
-  }, 0);
+function part2(input) {
+  return input
+    .split('\n\n')
+    .map(
+      g =>
+        g
+          .split(/\n/)
+          .map(a => a.split(''))
+          .reduce((a, b) => a.filter(x => b.includes(x))).length
+    )
+    .reduce((a, b) => a + b);
 }
