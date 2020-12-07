88daec51f4274599ae6cacaec0924a5b5fa920f3
Don Chea <donaldducky@gmail.com>
2020-12-06 23:00:16 -0500

    Simplify 2020 Day 5

--- 2020/src/day5/day5.ts
+++ 2020/src/day5/day5.ts
@@ -1,49 +1,34 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const lines = fs
+const input = fs
   .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
   .toString()
-  .trim()
-  .split('\n');
+  .trim();
 
-console.log('Part 1:', part1(lines));
-console.log('Part 2:', part2(lines));
+console.log('Part 1:', part1(input));
+console.log('Part 2:', part2(input));
 
-function part1(lines): number {
-  return Math.max(...lines.map(genId));
+function part1(input) {
+  return Math.max(...parse(input));
 }
 
-function part2(lines) {
-  const seatIds = lines.map(genId);
+function part2(input) {
+  const seatIds = parse(input);
   seatIds.sort((a, b) => a - b);
 
-  return (
-    seatIds.find((id, i) => {
-      return id + 1 != seatIds[i + 1];
-    }) + 1
-  );
+  return seatIds.find((id, i) => id + 1 != seatIds[i + 1]) + 1;
 }
 
-function genId(line) {
-  const parts = line.split('');
-
-  let row = binarySearch(parts.slice(0, 7), 127, 'F');
-  let col = binarySearch(parts.slice(7, 10), 7, 'L');
-
-  return row * 8 + col;
-}
-
-function binarySearch(input, upper, loChar) {
-  let min = 0;
-  let max = upper;
-  input.forEach(c => {
-    if (c === loChar) {
-      max = Math.floor(max - (max - min) / 2);
-    } else {
-      min = Math.ceil(min + (max - min) / 2);
-    }
-  });
-
-  return min;
+function parse(input) {
+  const tr = {
+    F: 0,
+    B: 1,
+    L: 0,
+    R: 1,
+  };
+  return input
+    .split('\n')
+    .map(s => s.replace(/./g, m => tr[m]))
+    .map(s => parseInt(s, 2));
 }
---
+++ 2020/src/day5/p1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+<input.txt | tr 'FLBR' '0011' | sort -nr | head -1 | awk '{print "ibase=2;"  $1}' | bc
