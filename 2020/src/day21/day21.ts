655ef4d5429a11d1d091c9a7e76c35b84fe96ccf
Don Chea <donaldducky@gmail.com>
2020-12-21 01:26:51 -0500

    Bit of clean up

--- 2020/src/day21/day21.ts
+++ 2020/src/day21/day21.ts
@@ -10,130 +10,69 @@ const lines = fs
   .readFileSync(path.join(__dirname, file), 'utf8')
   .toString()
   .trim()
-  .split('\n');
+  .split('\n')
+  .map(x => x.split(' (contains '))
+  .map(x => [x[0].split(' '), x[1].replace(/\)$/, '').split(', ')]);
 
-let p1 = part1(lines);
-console.log('Part 1:', p1);
-let p2 = part2(lines);
-console.log('Part 2:', p2);
+let mayContain = lines.reduce((acc, x) => {
+  return x[1].reduce((acc, a) => {
+    if (!(a in acc)) {
+      acc[a] = x[0];
+    } else {
+      acc[a] = acc[a].filter(v => x[0].includes(v));
+    }
 
-function part1(lines) {
-  lines = lines
-    .map(x => x.split(' (contains '))
-    .map(x => [x[0].split(' '), x[1].replace(/\)$/, '').split(', ')]);
-  console.log(lines);
+    return acc;
+  }, acc);
+}, {});
 
-  let allIngredients = lines.reduce((s, x) => {
-    return x[0].reduce((acc, x) => {
-      acc.add(x);
-      return acc;
-    }, s);
-  }, new Set());
-  console.log(allIngredients);
-
-  let mayContain = lines.reduce((acc, x) => {
-    return x[1].reduce((acc, a) => {
-      if (!(a in acc)) {
-        acc[a] = x[0];
-      } else {
-        acc[a] = acc[a].filter(v => x[0].includes(v));
-      }
-
-      return acc;
-    }, acc);
-  }, {});
-
-  console.log(mayContain);
-
-  mayContain = Object.entries(mayContain);
-  console.log(mayContain);
-
-  let allergens = {};
-
-  while (mayContain.length) {
-    let newMayContains = [];
-    for (let i = 0; i < mayContain.length; i++) {
-      let [a, xs] = mayContain[i];
-      console.log(a, xs, allergens);
-      xs = xs.filter(x => !allergens[x]);
-      console.log(a, xs, allergens);
-
+let entries: [string, string[]][] = Object.entries(mayContain);
+let allergens = {};
+while (entries.length) {
+  /* how to get rid of newMayContains intermediate value?
+   * entries will become empty, how to make this typescript stuff work?
+  entries = entries
+    .map(([a, xs]) => [a, xs.filter(x => !allergens[x])])
+    .filter(([a, xs]) => {
       if (xs.length === 1) {
         allergens[xs[0]] = a;
-      } else {
-        newMayContains.push([a, xs]);
+        return false;
       }
-    }
-    mayContain = newMayContains;
-  }
+      return true;
+    });
+    */
+  let newMayContains = [];
+  for (let i = 0; i < entries.length; i++) {
+    let [a, xs] = entries[i];
+    xs = xs.filter(x => !allergens[x]);
 
-  console.log('allergens', allergens);
+    if (xs.length === 1) {
+      allergens[xs[0]] = a;
+    } else {
+      newMayContains.push([a, xs]);
+    }
+  }
+  entries = newMayContains;
+}
+
+let p1 = part1(lines, allergens);
+console.log('Part 1:', p1);
+let p2 = part2(allergens);
+console.log('Part 2:', p2);
+
+function part1(lines, allergens) {
+  let allIngredients = new Set([...lines.flatMap(x => x[0])]);
   const notAllergens = new Set(
     [...allIngredients].filter(x => !(x in allergens))
   );
-  console.log(notAllergens);
+
   return lines
     .map(x => x[0])
     .flat()
     .filter(x => notAllergens.has(x)).length;
 }
 
-function part2(lines) {
-  lines = lines
-    .map(x => x.split(' (contains '))
-    .map(x => [x[0].split(' '), x[1].replace(/\)$/, '').split(', ')]);
-  console.log(lines);
-
-  let allIngredients = lines.reduce((s, x) => {
-    return x[0].reduce((acc, x) => {
-      acc.add(x);
-      return acc;
-    }, s);
-  }, new Set());
-  console.log(allIngredients);
-
-  let mayContain = lines.reduce((acc, x) => {
-    return x[1].reduce((acc, a) => {
-      if (!(a in acc)) {
-        acc[a] = x[0];
-      } else {
-        acc[a] = acc[a].filter(v => x[0].includes(v));
-      }
-
-      return acc;
-    }, acc);
-  }, {});
-
-  console.log(mayContain);
-
-  mayContain = Object.entries(mayContain);
-  console.log(mayContain);
-
-  let allergens = {};
-
-  while (mayContain.length) {
-    let newMayContains = [];
-    for (let i = 0; i < mayContain.length; i++) {
-      let [a, xs] = mayContain[i];
-      console.log(a, xs, allergens);
-      xs = xs.filter(x => !allergens[x]);
-      console.log(a, xs, allergens);
-
-      if (xs.length === 1) {
-        allergens[xs[0]] = a;
-      } else {
-        newMayContains.push([a, xs]);
-      }
-    }
-    mayContain = newMayContains;
-  }
-
-  console.log('allergens', allergens);
-  const notAllergens = new Set(
-    [...allIngredients].filter(x => !(x in allergens))
-  );
-  console.log(notAllergens);
-
+function part2(allergens) {
   let k = Object.entries(allergens);
   k.sort((a, b) => {
     if (a[1] > b[1]) {
@@ -144,7 +83,6 @@ function part2(lines) {
 
     return 0;
   });
-  console.log(k);
 
   return k.map(x => x[0]).join(',');
 }
