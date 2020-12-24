d174fdc01f820df4d319204d624b3fa9ca221c94
Don Chea <donaldducky@gmail.com>
2020-12-24 01:53:48 -0500

    Bit of clean up

--- 2020/src/day24/day24.ts
+++ 2020/src/day24/day24.ts
@@ -13,6 +13,23 @@ const NW = '⬉';
 const W = 'w';
 const E = 'e';
 
+const NEIGHBOURS_OFFSET = {
+  [E]: [1, 0],
+  [W]: [-1, 0],
+  [SE]: [1, 1],
+  [SW]: [0, 1],
+  [NE]: [1, -1],
+  [NW]: [0, -1],
+};
+const NEIGHBOURS = {
+  [E]: [1, 0],
+  [W]: [-1, 0],
+  [SE]: [0, 1],
+  [SW]: [-1, 1],
+  [NE]: [0, -1],
+  [NW]: [-1, -1],
+};
+
 const lines = fs
   .readFileSync(path.join(__dirname, file), 'utf8')
   .toString()
@@ -30,27 +47,29 @@ let p2 = part2(lines);
 console.log('Part 2:', p2);
 
 function part1(lines) {
-  //console.log(lines);
-
-  return lines.reduce((t, line) => {
-    let id = line.reduce((p, d) => move(p, d), [0, 0]);
-    let k = id.join(',');
-
-    if (t.has(k)) {
-      t.delete(k);
-      //console.log(id, k, 'flip back to white');
-    } else {
-      t.add(k);
-      //console.log(id, k, 'flip to black');
-    }
-
-    return t;
-  }, new Set()).size;
+  return renovate(lines).size;
 }
 
 function part2(lines) {
-  let tiles = lines.reduce((t, line) => {
-    let id = line.reduce((p, d) => move(p, d), [0, 0]);
+  let tiles = renovate(lines);
+
+  const coords = [...tiles.keys()].map(x => x.split(','));
+  const xs = coords.map(x => x[0]);
+  const ys = coords.map(x => x[1]);
+  const minX = Math.min(...xs);
+  const maxX = Math.max(...xs);
+  const minY = Math.min(...ys);
+  const maxY = Math.max(...ys);
+  for (let i = 0; i < 100; i++) {
+    tiles = step(tiles, minX - i - 2, maxX + i + 2, minY - i - 2, maxY + i + 2);
+  }
+
+  return tiles.size;
+}
+
+function renovate(lines) {
+  return lines.reduce((t, line) => {
+    let id = line.reduce(([x, y], d) => move(x, y, d), [0, 0]);
     let k = id.join(',');
 
     if (t.has(k)) {
@@ -61,108 +80,41 @@ function part2(lines) {
 
     return t;
   }, new Set());
+}
 
-  console.log(tiles);
-  const coords = [...tiles.keys()].map(x => x.split(','));
-  const xs = coords.map(x => x[0]);
-  const ys = coords.map(x => x[1]);
-  const minX = Math.min(...xs);
-  const maxX = Math.max(...xs);
-  const minY = Math.min(...ys);
-  const maxY = Math.max(...ys);
-  console.log(coords, minX, maxX, minY, maxY);
-
-  for (let i = 0; i < 100; i++) {
-    let tiles2 = new Set();
-    for (let x = minX - i - 100; x < maxX + i + 100; x++) {
-      for (let y = minY - i - 100; y < minY + i + 100; y++) {
-        let n = neighbours(x, y).filter(p => tiles.has(p.join(','))).length;
-        let t = [x, y].join(',');
-        if (tiles.has(t)) {
-          if (n === 1 || n === 2) {
-            tiles2.add(t);
-          }
-        } else {
-          if (n === 2) {
-            tiles2.add(t);
-          }
+function step(tiles, x1, x2, y1, y2) {
+  let tiles2 = new Set();
+  for (let x = x1; x < x2; x++) {
+    for (let y = y1; y < y2; y++) {
+      let n = neighbours(x, y).filter(p => tiles.has(p.join(','))).length;
+      let t = [x, y].join(',');
+      if (tiles.has(t)) {
+        if (n === 1 || n === 2) {
+          tiles2.add(t);
+        }
+      } else {
+        if (n === 2) {
+          tiles2.add(t);
         }
       }
     }
-    tiles = tiles2;
   }
 
-  return tiles.size;
+  return tiles2;
 }
 
-function move(p, d) {
-  let [x, y] = p;
-  const offset = Math.abs(p[1]) % 2;
-  switch (d) {
-    case E:
-      x++;
-      break;
-    case W:
-      x--;
-      break;
-    case SE:
-      if (offset) {
-        x++;
-        y++;
-      } else {
-        y++;
-      }
-      break;
-    case SW:
-      if (offset) {
-        y++;
-      } else {
-        x--;
-        y++;
-      }
-      break;
-    case NE:
-      if (offset) {
-        x++;
-        y--;
-      } else {
-        y--;
-      }
-      break;
-    case NW:
-      if (offset) {
-        y--;
-      } else {
-        x--;
-        y--;
-      }
-      break;
-    default:
-      throw new Error(`unknown ${d}`);
-  }
+function move(x, y, d) {
+  let dv = isOffset(x, y) ? NEIGHBOURS_OFFSET[d] : NEIGHBOURS[d];
 
-  return [x, y];
+  return [x + dv[0], y + dv[1]];
+}
+
+function isOffset(x, y) {
+  return Math.abs(y) % 2;
 }
 
 function neighbours(x, y) {
-  const offset = Math.abs(y) % 2;
-  if (offset) {
-    return [
-      [x + 1, y],
-      [x - 1, y],
-      [x + 1, y + 1],
-      [x, y + 1],
-      [x + 1, y - 1],
-      [x, y - 1],
-    ];
-  } else {
-    return [
-      [x + 1, y],
-      [x - 1, y],
-      [x, y + 1],
-      [x - 1, y + 1],
-      [x, y - 1],
-      [x - 1, y - 1],
-    ];
-  }
+  return Object.values(
+    isOffset(x, y) ? NEIGHBOURS_OFFSET : NEIGHBOURS
+  ).map(([dx, dy]) => [x + dx, y + dy]);
 }
