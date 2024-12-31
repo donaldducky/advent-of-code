b8459872a246b65c2dab721f62a1ebb2942f6df9
Don Chea <donaldducky@gmail.com>
2024-12-31 00:56:55 -0500

    2024.20

--- aoc_helpers/lib/aoc_helpers.ex
+++ aoc_helpers/lib/aoc_helpers.ex
@@ -23,6 +23,32 @@ defmodule AocHelpers do
 
   def map_ints(lines), do: lines |> Enum.map(&String.to_integer/1)
 
+  def grid(input) do
+    lines =
+      lines(input)
+      |> Enum.map(&String.split(&1, "", trim: true))
+
+    h = lines |> Enum.count()
+    w = hd(lines) |> Enum.count()
+
+    grid =
+      for {row, y} <- Enum.with_index(lines),
+          {cell, x} <- Enum.with_index(row),
+          into: %{},
+          do: {{x, y}, cell}
+
+    {grid, w, h}
+  end
+
+  def find_pos(grid, val) do
+    grid
+    |> Enum.find(fn {_, v} -> v == val end)
+    |> case do
+      nil -> nil
+      {pos, _} -> pos
+    end
+  end
+
   def draw_grid(grid, w, h) do
     for y <- 0..(h - 1), x <- 0..(w - 1), reduce: "" do
       acc ->
@@ -50,6 +76,10 @@ defmodule AocHelpers do
 
   def neighbours(), do: [{1, 0}, {-1, 0}, {0, 1}, {0, -1}]
   def neighbours({x, y}), do: neighbours() |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
+
+  def neighbours({x, y}, n) when n > 0 do
+    for dx <- -n..n, dy <- -n..n, abs(dx) + abs(dy) <= n, {dx, dy} != {0, 0}, do: {x + dx, y + dy}
+  end
 
   def manhattan_dist({x1, y1}, {x2, y2}), do: abs(x1 - x2) + abs(y1 - y2)
 end
